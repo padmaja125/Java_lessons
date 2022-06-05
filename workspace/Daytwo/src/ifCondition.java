59b765c2169533ec6abe70ec0db791dfc9d0000a
Padmaja Ramesh <62902780+padmaja125@users.noreply.github.com>
2022-06-05 16:17:24 -0400

    if condition commit

---
+++ workspace/Daytwo/src/ifCondition.java
@@ -0,0 +1,15 @@
+
+public class ifCondition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10, b= 20;
+		if(a > b) {
+			System.out.println("a is greater " + a);
+		}else {
+			System.out.println("b is greater " + b);
+		}
+
+	}
+
+}
